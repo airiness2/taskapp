b03d4d42a264f96b444aaee24fbc9363e67046b0
reiko watanabe <blackgothy@gmail.com>
2019-06-29 16:36:25 +0900

    fix: changed column names

--- db/migrate/20190629073444_create_tasks.rb
+++ db/migrate/20190629073444_create_tasks.rb
@@ -1,8 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :taskname
-      t.string :taskdetail
+      t.string :name
+      t.string :detail
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_29_064018) do
+ActiveRecord::Schema.define(version: 2019_06_29_073444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "taskname"
-    t.string "taskdetail"
+    t.string "name"
+    t.string "detail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
