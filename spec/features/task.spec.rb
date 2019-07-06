362d5d1ee680e1b34c68ca98651b4f60449ab762
reiko watanabe <blackgothy@gmail.com>
2019-07-06 21:50:38 +0900

    fix: spec file

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -18,20 +18,13 @@ RSpec.feature "タスク管理機能", type: :feature do
   end
 
   scenario "タスク作成のテスト" do
-    # new_task_pathにvisitする（タスク登録ページに遷移する）
     visit new_task_path
 
-    # 「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄に
-    # タスクのタイトルと内容をそれぞれfill_in（入力）する
     fill_in 'task_name', with: 'test_task_03'
     fill_in 'task_detail', with: 'task_detail_03'
 
-    #save_and_open_page
-    # 「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）
     click_on '登録する'
 
-    # clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
-    # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
     expect(page).to have_content 'task_detail_03'
   end
 
@@ -40,7 +33,9 @@ RSpec.feature "タスク管理機能", type: :feature do
 
     fill_in 'task_name', with: 'test_task_04',  match: :first
     fill_in 'task_detail', with: 'task_detail_04', match: :first
-    fill_in 'task_endtime', with: '2019-07-06 20:00:00 +0900', match: :first
+#    fill_in 'task_endtime', with: '2019/07/06 20:00:00', match: :first
+#    select '2019-07-06 20:00:00', :from => 'task_endtime', match: :first
+
 
     click_on '登録する'
 
@@ -53,7 +48,7 @@ RSpec.feature "タスク管理機能", type: :feature do
   scenario "タスクが作成日時の降順に並んでいるかのテスト" do
     visit tasks_path
 
-    expect(page).to have_text 'test_task_02 samplesample 2019-12-06 12:00:00 +0900 詳細 編集 削除 test_task_01 testtesttest 2019-10-06 12:00:00 +0900 詳細 編集 削除'
+    expect(page).to have_text 'test_task_02 samplesample 2015/12/06 12:00 詳細 編集 削除 test_task_01 testtesttest 2017/10/06 12:00 詳細 編集 削除'
   end
 
 
@@ -62,13 +57,12 @@ RSpec.feature "タスク管理機能", type: :feature do
 
     fill_in 'task_name', with: 'test_task_05'
     fill_in 'task_detail', with: 'task_detail_05'
-    fill_in 'task_endtime', with: '2015-07-06 20:00:00 +0900'
-
+#    select '2020-10-06', :from => 'task_endtime'
     click_on '登録する'
 
     click_on '終了期限でソートする'
 
-    expect(page).to have_text 'test_task_02 samplesample 2019-12-06 12:00:00 +0900 詳細 編集 削除 test_task_01 testtesttest 2019-10-06 12:00:00 +0900 詳細 編集 削除'
+    expect(page).to have_text 'test_task_05 task_detail_05 2020/10/06 12:00 詳細 編集 削除 test_task_01 testtesttest 2017/10/06 12:00 詳細 編集 削除'
   end
 
 
