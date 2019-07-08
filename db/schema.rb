2034797c910dda3d28a623696df60fb4755dc5f7
reiko watanabe <blackgothy@gmail.com>
2019-07-08 23:38:49 +0900

    feat: added index to name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_08_062928) do
+ActiveRecord::Schema.define(version: 2019_07_08_141711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_07_08_062928) do
     t.datetime "updated_at", null: false
     t.datetime "endtime", default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.string "status"
+    t.index ["name"], name: "index_tasks_on_name"
   end
 
 end
