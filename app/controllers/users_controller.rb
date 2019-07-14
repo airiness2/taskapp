5b306377825698b7b8b1426ab33ce12572371b85
reiko watanabe <blackgothy@gmail.com>
2019-07-14 18:48:32 +0900

    feat: ログイン時にユーザ登録画面に飛ばさない、自分以外のマイページに飛ばさない機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,14 +15,24 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if current_user.id != @user.id
+      redirect_to root_path, notice: "閲覧権限がありません"
+    end
   end
-
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                     :password_confirmation)
+  end
+
+  def ensure_correct_user
+    @picture = Picture.find_by(id: params[:id])
+    if current_user.id != @picture.user_id
+      flash[:notice] = "権限がありません"
+      redirect_to root_path
+    end
   end
 
 end
