24ee302c8205df9a1cc1ee3346d3048e606a1dd7
reiko watanabe <blackgothy@gmail.com>
2019-07-14 16:58:10 +0900

    feat: added user controller and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
 
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
-                  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+                  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                  uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
 
   has_secure_password
