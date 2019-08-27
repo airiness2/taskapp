a98ae47269a6e75da91a663ec0a00d6a92df7fdf
reiko watanabe <blackgothy@gmail.com>
2019-08-27 23:17:34 +0900

    feat: ラベル編集、削除機能実装

--- app/controllers/label_controller.rb
+++ app/controllers/label_controller.rb
@@ -1,4 +1,7 @@
 class LabelController < ApplicationController
+
+  before_action :set_label, only: [:edit, :update, :destroy]
+
   def new
     @label = Label.new
   end
@@ -18,9 +21,30 @@ class LabelController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @label.update(label_params)
+      redirect_to label_index_path, notice: "ラベルを編集しました！"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @label.destroy
+    redirect_to label_index_path, notice: "ラベルを削除しました！"
+  end
+
+
   private
 
   def label_params
     params.require(:label).permit(:name, :user_id)
+  end
+
+  def set_label
+    @label = Label.find(params[:id])
   end
 end
